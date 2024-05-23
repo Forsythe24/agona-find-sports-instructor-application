e82e5a3f7901c6dba23159de5c83349560f805e5
Forsythe24 <miron.solopov@yandex.ru>
2024-05-24 00:28:12 +0300

    fix architecture (migrate some functions to view model) + bug fixing + add "no chats" text view

--- feature-chat-impl/src/main/java/com/solopov/feature_chat_impl/presentation/chat_list/ChatsViewModel.kt
+++ feature-chat-impl/src/main/java/com/solopov/feature_chat_impl/presentation/chat_list/ChatsViewModel.kt
@@ -2,7 +2,9 @@ package com.solopov.feature_chat_impl.presentation.chat_list
 
 import androidx.lifecycle.viewModelScope
 import com.solopov.common.base.BaseViewModel
+import com.solopov.common.core.resources.ResourceManager
 import com.solopov.common.model.ChatCommon
+import com.solopov.common.utils.DateFormatter
 import com.solopov.common.utils.ExceptionHandlerDelegate
 import com.solopov.common.utils.runCatching
 import com.solopov.feature_chat_api.domain.interfaces.ChatInteractor
@@ -13,12 +15,16 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import java.util.Calendar
+import java.util.Date
 
 class ChatsViewModel(
     private val interactor: ChatInteractor,
     private val exceptionHandlerDelegate: ExceptionHandlerDelegate,
     private val chatMappers: ChatMappers,
     private val router: ChatRouter,
+    private val resManager: ResourceManager,
+    private val dateFormatter: DateFormatter
 ) : BaseViewModel() {
     private val _chatsFlow = MutableStateFlow<List<ChatItem>?>(null)
     val chatsFlow: StateFlow<List<ChatItem>?>
@@ -50,6 +56,35 @@ class ChatsViewModel(
             }
         }
 
+    }
+
+    fun date(chats: List<ChatItem>): List<ChatItem> {
+
+        chats.map { chatItem ->
+            val date = dateFormatter.parseStringToDateTime(chatItem.lastMessageDate!!)!!
+            val dateString = dateFormatter.formatDate(date)
+            val now = Date()
+            val todayDateString = dateFormatter.formatDate(now)
+
+            if (dateString == todayDateString) {
+                chatItem.userFriendlyLastMessageDate =
+                    dateFormatter.formatDateTime(date).split(" ")[1]
+            }
+
+            val c1 = Calendar.getInstance();
+            c1.add(Calendar.DAY_OF_YEAR, -1); // yesterday
+
+            val c2 = Calendar.getInstance();
+            c2.time = date;
+
+            if (c1.get(Calendar.YEAR) == c2.get(Calendar.YEAR)
+                && c1.get(Calendar.DAY_OF_YEAR) == c2.get(Calendar.DAY_OF_YEAR)
+            ) {
+                chatItem.userFriendlyLastMessageDate =
+                    resManager.getString(com.solopov.feature_chat_impl.R.string.yesterday)
+            }
+        }
+        return chats
     }
 
     fun setUser() {
